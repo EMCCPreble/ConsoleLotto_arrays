dbc7b4a6a81a28357b687d3fb9ef5005954726c1
EMCCPreble <38023446+EMCCPreble@users.noreply.github.com>
2018-12-07 08:29:42 -0500

    Create LottoNumbers.java

---
+++ LottoNumbers.java
@@ -0,0 +1,12 @@
+/***********
+ * TODO
+ * Generate 8 random numbers between 1 and 90
+ * Store numbers in an array
+ * Output the numbers to the user in a way that makes them feel like something hard is happening
+ * Fill the arraylist with only unique numbers
+ * add error catches
+ */
+
+public class LottoNumbers {
+
+}
